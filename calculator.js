dd0d8be7c499741a57616698cb941e73b1faac69
Jamie Bryson <jamieab98@gmail.com>
2025-07-23 16:47:43 -0400

    added a splice so the console will only print the most recent equation

--- calculator.js
+++ calculator.js
@@ -1,28 +1,3 @@
-/*function addition(a, b) {
-    let sum = a + b;
-    console.log(a, "+", b, "=", sum)
-}
-addition(2, 3)
-
-function subtraction(a, b) {
-    let difference = a - b;
-    console.log(a, "-", b, "=", difference)
-}
-subtraction(2, 3)
-
-function multiply(a, b) {
-    let product = a * b;
-    console.log(a, "*", b, "=", product)
-}
-multiply(2, 3)
-
-function divide(a, b) {
-    let quotient = a / b;
-    console.log(a, "/", b, "=", quotient)
-}
-divide(2, 3)
-*/
-
 let history = [];
 let equation = '';
 
@@ -33,27 +8,26 @@ function expression(a, operator, b) {
         history.push(equation)
     } else if (operator === "-") {
         let result = a - b;
-        history.push(a, operator, b, "=", result)
+        equation = a + " - " + b + " = " + result;
+        history.push(equation)
     } else if (operator === "*") {
         let result = a * b;
-        history.push(a, operator, b, "=", result)
+        equation = a + " * " + b + " = " + result;
+        history.push(equation)
     } else if (operator === "/") {
         if (b == 0) {
             console.log("Error. Cannot divide by 0")
         } else {
         let result = a / b;
-        history.push(a, operator, b, "=", result)
+        equation = a + " / " + b + " = " + result;
+        history.push(equation)
         }
     } else {
         console.log('Error. Please enter a valid operator "+", "-", "*", or "/".')
     }
-    //let historySpaceSeparated = history.join(' ');
     console.log(history);
+    history.splice(0,1);
 }
 
 expression(2, "+", 7)
-expression(15, "+", 43)
-//let listOfNames = [];
-//listOfNames.push("Austin");
-//listOfNames.push("A + B = C");
-//console.log(listOfNames)
+expression(15, "/", 43)
