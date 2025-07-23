6f3ec8b1fed078ccf2b6894c85112df3a86c8450
Jamie Bryson <jamieab98@gmail.com>
2025-07-22 22:06:53 -0400

    created one function for each operation

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function addition(a, b) {
+    let sum = a + b;
+    console.log(a, "+", b, "=", sum)
+}
+addition(2, 3)
+
+function subtraction(a, b) {
+    let difference = a - b;
+    console.log(a, "-", b, "=", difference)
+}
+subtraction(2, 3)
+
+function multiply(a, b) {
+    let product = a * b;
+    console.log(a, "*", b, "=", product)
+}
+multiply(2, 3)
+
+function divide(a, b) {
+    let quotient = a / b;
+    console.log(a, "/", b, "=", quotient)
+}
+divide(2, 3)
