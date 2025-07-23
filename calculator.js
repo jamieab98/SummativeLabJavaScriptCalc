3256d9b51feb930e06735e874b87787250298fae
Jamie Bryson <jamieab98@gmail.com>
2025-07-23 16:43:14 -0400

    set up the history array

--- calculator.js
+++ calculator.js
@@ -1,50 +1,59 @@
-function addition(a, b) {
+/*function addition(a, b) {
     let sum = a + b;
     console.log(a, "+", b, "=", sum)
 }
-//addition(2, 3)
+addition(2, 3)
 
 function subtraction(a, b) {
     let difference = a - b;
     console.log(a, "-", b, "=", difference)
 }
-//subtraction(2, 3)
+subtraction(2, 3)
 
 function multiply(a, b) {
     let product = a * b;
     console.log(a, "*", b, "=", product)
 }
-//multiply(2, 3)
+multiply(2, 3)
 
 function divide(a, b) {
     let quotient = a / b;
     console.log(a, "/", b, "=", quotient)
 }
-//divide(2, 3)
+divide(2, 3)
+*/
+
+let history = [];
+let equation = '';
 
 function expression(a, operator, b) {
     if (operator === "+") {
         let result = a + b;
-        console.log(a, operator, b, "=",result)
+        equation = a + " + " + b + " = " + result;
+        history.push(equation)
     } else if (operator === "-") {
         let result = a - b;
-        console.log(a, operator, b, "=", result)
+        history.push(a, operator, b, "=", result)
     } else if (operator === "*") {
         let result = a * b;
-        console.log(a, operator, b, "=", result)
+        history.push(a, operator, b, "=", result)
     } else if (operator === "/") {
+        if (b == 0) {
+            console.log("Error. Cannot divide by 0")
+        } else {
         let result = a / b;
-        console.log(a, operator, b, "=", result)
+        history.push(a, operator, b, "=", result)
+        }
     } else {
         console.log('Error. Please enter a valid operator "+", "-", "*", or "/".')
     }
-}
-
-//let history = [];
-function updateHistory(update) {
-    let history = [];
-    history[0] = update;
+    //let historySpaceSeparated = history.join(' ');
     console.log(history);
 }
-updateHistory("2 / 4 = 0.5")
-updateHistory("5 + 3 = 8")
+
+expression(2, "+", 7)
+expression(15, "+", 43)
+//let listOfNames = [];
+//listOfNames.push("Austin");
+//listOfNames.push("A + B = C");
+//console.log(listOfNames)
